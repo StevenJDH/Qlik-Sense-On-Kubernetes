f24125b9bd42c5bac66f545977c92465ac39bf22
Steven Jenkins De Haro <stevendeharo@gmail.com>
2019-07-31 22:20:09 +0200

    Updated for upstream sync

--- vagrantProvision/sh/3_minikube.sh
+++ vagrantProvision/sh/3_minikube.sh
@@ -45,6 +45,7 @@ touch /home/vagrant/.kube/config
 echo 'Starting minikube local cluster'
 
 minikube start --kubernetes-version v1.15.0 --memory 8000 --cpus=2 --vm-driver=none
+# minikube start --kubernetes-version v1.15.0 --memory 8000 --cpus=2 --vm-driver=none --extra-config=apiserver.service-node-port-range=80-32767
 
 #sudo cp /root/.kube $HOME/.kube
 sudo chown -R vagrant /home/vagrant/.kube
